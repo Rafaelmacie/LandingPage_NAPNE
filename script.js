3035d75930767aa64784baff90d2dd2dd985683c
Samelafarias <124313487+Samelafarias@users.noreply.github.com>
2024-09-05 10:59:03 -0300

    + ajustes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//Script de filtro para a mudança automática de cores
+//Script para a mudança de cor por meio de filtro
+//ainda precisa de alguns ajustes
 document.querySelector("select[name='select-color']").addEventListener("change", function() {
     const body = document.body;
+    console.log("Cor selecionada:", this.value); // Verifica o valor selecionado
     body.className = ""; // Reseta a classe do body
     body.classList.add(this.value); // Adiciona a classe correspondente à visão selecionada
 });
